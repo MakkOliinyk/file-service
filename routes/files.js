82701d470d9d784381dd876ccb35de4bb6992075
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 14:56:08 +0300

    Add routes

---
+++ routes/files.js
@@ -0,0 +1,86 @@
+const routes = async (fastify) => {
+    fastify.post('/add', async (request, reply) => {
+        const data = await request.file();
+        const { filename, file, userId } = data.fields;
+
+        const uploadParams = {
+            Bucket: 'YOUR_BUCKET_NAME',
+            Key: filename,
+            Body: file,
+            Metadata: {
+                'userId': userId,
+            },
+        };
+
+        try {
+            const uploadResult = await fastify.s3.upload(uploadParams).promise();
+            console.log(`Success: File uploaded successfully to ${uploadResult.Location}`);
+            reply.send({ message: 'File uploaded successfully' });
+        } catch (error) {
+            console.error('Error: Failed to upload file', error);
+            reply.status(500).send({ error: 'Error: Failed to upload file' });
+        }
+    });
+
+    fastify.get('/getAll/:userId', async (request, reply) => {
+        const { userId } = request.params;
+
+        const listParams = {
+            Bucket: 'YOUR_BUCKET_NAME',
+            Prefix: '',
+        };
+
+        try {
+            const listResult = await fastify.s3.listObjectsV2(listParams).promise();
+            const files = listResult.Contents.filter((file) => file.Metadata.userId === userId);
+            reply.send({ files });
+        } catch (error) {
+            console.error('Error: Failed to retrieve files', error);
+            reply.status(500).send({ error: 'Error: Failed to retrieve files' });
+        }
+    });
+
+    fastify.delete('/delete/:fileId', async (request, reply) => {
+        const { fileId } = request.params;
+
+        const deleteParams = {
+            Bucket: 'YOUR_BUCKET_NAME',
+            Key: fileId,
+        };
+
+        try {
+            await fastify.s3.deleteObject(deleteParams).promise();
+            console.log(`Success: File with ID ${fileId} deleted successfully`);
+            reply.send({ message: 'Success: File deleted successfully' });
+        } catch (error) {
+            console.error('Error: Failed to delete file', error);
+            reply.status(500).send({ error: 'Error: Failed to delete file' });
+        }
+    });
+
+    fastify.get('/get/:fileId', async (request, reply) => {
+        const { fileId } = request.params;
+
+        const downloadParams = {
+            Bucket: 'YOUR_BUCKET_NAME',
+            Key: fileId,
+        };
+
+        try {
+            const downloadResult = await fastify.s3.getObject(downloadParams).promise();
+            const { Body } = downloadResult;
+
+            const fileStream = fastify.s3.getObject(downloadParams).createReadStream();
+
+            reply.header('Content-Disposition', `attachment; filename=${fileId}`);
+            reply.type('application/octet-stream');
+            fileStream.pipe(reply.res);
+        } catch (error) {
+            console.error('Error: Failed to download file', error);
+            reply.status(500).send({ error: 'Error: Failed to download file' });
+        }
+    });
+};
+
+export default routes;
+
